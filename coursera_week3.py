8d76e9f7af9432ad9ec61b5d2bbf15836507dc5e
Soha <soha00hussain@gmail.com>
2021-07-10 20:52:46 +0530

    heatmaps

--- coursera_week3.py
+++ coursera_week3.py
@@ -177,4 +177,21 @@ ax2.yaxis.tick_right()
 plt.figure()
 _ = plt.boxplot([ df['normal'], df['random'], df['gamma'] ] )
 
+# Heatmaps
+
+# Heatmaps are a way to visualize three-dimensional data and to take advantage of spatial proximity of those dimensions.
+
+plt.figure()
+
+Y = np.random.normal(loc=0.0, scale=1.0, size=10000)
+X = np.random.random(size=10000)
+_ = plt.hist2d(X, Y, bins=25)
+
+plt.figure()
+_ = plt.hist2d(X, Y, bins=100)
+
+# add a colorbar legend
+plt.colorbar()
+
+
 
